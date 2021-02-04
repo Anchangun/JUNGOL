019216bb7339f1c8b0a541589c53c3f2abfa122c
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-02-04 09:40:47 +0900

    573. question update

--- Language_Coder/573. 자가진단-3/Solution.cpp
+++ Language_Coder/573. 자가진단-3/Solution.cpp
@@ -3,11 +3,12 @@
 void Solution::run(int size)
 {
 	int enter = size;
+	int check= enter;
 	int lp_size = size * size;
 	for (int lp = 1; lp < lp_size+1; lp++) {
 		if (enter == lp) {
 			std::cout << lp << std::endl;
-			enter = enter + enter;
+			enter = enter + check;
 		}
 		else
 		std::cout << lp << " ";
