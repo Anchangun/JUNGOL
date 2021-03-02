62334417632c408464e60db7c8836ab53e176279
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-03-02 13:34:41 +0900

    Create : Language_Coder/05. Struct/613. 구조체

---
+++ Language_Coder/05. Struct/613.구조체 - 자가진단1/quest.h
@@ -0,0 +1,17 @@
+#pragma once
+/*
+613. 구조체 - 자가진단1
+"이름", "회사명", "사원번호"를 입력받아 아래와 같이 출력하는 프로그램을 작성하시오.
+
+(이름과 회사명은 각각 20자 이하이다.)
+
+Input :
+Changun AN
+Tistory
+4
+
+Output :
+Name : Changun AN
+Company : Tistory
+Number : 4
+*/
