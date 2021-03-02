aa710741b50c3f79ce1a7867cf915d8cb20c37aa
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-03-02 14:17:55 +0900

    Solution : 613. 구조체 - 자가진단1

--- Language_Coder/05. Struct/613.구조체 - 자가진단1/Solution.cpp
+++ Language_Coder/05. Struct/613.구조체 - 자가진단1/Solution.cpp
@@ -1,1 +1,31 @@
 #include "Solution.h"
+
+void Solution::data_input(){
+	std::cout << "What you're name?" << std::endl;
+	std::cin >> cp->name;
+	if (cp->name.size() > 20) {
+		std::cout << "error"<<std::endl;
+		exit(0);
+	}
+	std::cout << "What is the name of your company?"<<std::endl;
+	std::cin >> cp->company_name;
+	if (cp->company_name.size() > 20) {
+		std::cout << "error" << std::endl;
+		exit(0);
+	}
+	std::cout << "What's your company number?" << std::endl;
+	std::cin >> cp->number;
+}
+
+void Solution::data_output(){
+	if (cp->company_name.size() > 0 && cp->name.size() > 0) {
+		std::cout << "Name : " << cp->name<<std::endl;
+		std::cout << "Company : " << cp->company_name << std::endl;
+		std::cout << "Number : " << cp->number << std::endl;
+	}
+}
+
+void Solution::run(){
+	data_input();
+	data_output();
+}
--- Language_Coder/05. Struct/613.구조체 - 자가진단1/Solution.h
+++ Language_Coder/05. Struct/613.구조체 - 자가진단1/Solution.h
@@ -1,5 +1,21 @@
 #pragma once
+
+#include<iostream>
+typedef struct company {
+	std::string name;
+	std::string company_name;
+	int number;
+}Comapny;
 class Solution
 {
+private :
+	Comapny* cp;
+	void data_input();
+	void data_output();
+public :
+	Solution() {
+		cp = new Comapny();
+	}
+	void run();
 };
 
