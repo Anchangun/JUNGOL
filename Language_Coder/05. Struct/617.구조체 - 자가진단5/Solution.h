4a07b3a7f866a0540a142fbc8e55caacf5bc3f11
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-03-09 13:26:53 +0900

    Revert "Solution : 문제 해결"
    This reverts commit 035d452ee1c8d21e5bb182e5a6f2f2d32a447572.

--- Language_Coder/05. Struct/617.구조체 - 자가진단5/Solution.h
+++ Language_Coder/05. Struct/617.구조체 - 자가진단5/Solution.h
@@ -1,25 +1,7 @@
 #pragma once
-
-#include<iostream>
-#include<vector>
-typedef struct USER_DATA {
-	std::string name;
-	float tall;
-}USER_DATA;
 class Solution
 {
-
-private :
-	int user_data_cnt;
-	std::vector<USER_DATA> vector_data;
-	USER_DATA* min_data;
-	void fn_user_input();
-	void fn_user_input_data();
-	void fn_min_check();
-	void fn_output_data();
-
 public :
-	Solution();
 	void run();
 };
 
