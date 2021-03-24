375132a7d0411837d2ea82363873ce4d37509ad1
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-03-24 10:10:50 +0900

    Update : 207. 파일입출력 - 형성평가3 문제 푸는 中

--- Language_Coder/06. FileStream/207. 파일입출력 - 형성평가3/cl_solution.h
+++ Language_Coder/06. FileStream/207. 파일입출력 - 형성평가3/cl_solution.h
@@ -1,5 +1,16 @@
 #pragma once
+#include<iostream>
+typedef struct UserData
+{
+	int first_data;
+	int second_data;
+	std::string calculations;
+}UserData;
+
 class cl_solution
 {
+private :
+	UserData st_user_data;
+	void input_data();
 };
 
