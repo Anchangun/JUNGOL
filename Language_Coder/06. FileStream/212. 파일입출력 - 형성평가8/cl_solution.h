585bc11239b1b4017a61f0b204d0485d897601de
DESKTOP-BVI2QKE\wavem <changun516@gmail.com>
2021-03-25 17:29:22 +0900

    Update : 212. 파일입출력 - 형성평가8

--- Language_Coder/06. FileStream/212. 파일입출력 - 형성평가8/cl_solution.h
+++ Language_Coder/06. FileStream/212. 파일입출력 - 형성평가8/cl_solution.h
@@ -1,5 +1,21 @@
 #pragma once
+#include"share.h"
+#include <vector>
+typedef struct UserData {
+	int num[3];
+}UserData;
+
 class cl_solution
 {
+private :
+
+	std::vector<UserData> vt_user_data;
+	void fn_input_data();
+	void fn_result_data();
+	bool fn_data_check(int data);
+
+public :
+
+	void fn_run();
 };
 
